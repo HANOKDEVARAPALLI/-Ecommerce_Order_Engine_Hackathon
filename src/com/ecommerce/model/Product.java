65ad8e0e4094a43eac8d8c8e9937554fc25c6096
HANOKDEVARAPALLI <hanokdevarapalli@gmail.com>
2026-03-30 17:22:38 +0530

    Added Product model class

--- src/com/ecommerce/model/Product.java
+++ src/com/ecommerce/model/Product.java
@@ -1,32 +1,43 @@
 package com.ecommerce.model;
 
 public class Product {
-    private int id;
-    private String name;
-    private double price;
-    private int stock;
+	private int id;
+	private String name;
+	private double price;
+	private int stock;
 
-    public Product(int id, String name, double price, int stock) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-    }
+	public Product(int id, String name, double price, int stock) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.stock = stock;
+	}
 
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public double getPrice() { return price; }
-    public int getStock() { return stock; }
+	public int getId() {
+		return id;
+	}
 
-    public void setStock(int stock) {
-        if(stock < 0){
-            throw new IllegalArgumentException("Stock cannot be negative");
-        }
-        this.stock = stock;
-    }
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public String toString() {
-        return id + " | " + name + " | ₹" + price + " | Stock: " + stock;
-    }
+	public double getPrice() {
+		return price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		if (stock < 0) {
+			throw new IllegalArgumentException("Stock cannot be negative");
+		}
+		this.stock = stock;
+	}
+
+	@Override
+	public String toString() {
+		return id + " | " + name + " | ₹" + price + " | Stock: " + stock;
+	}
 }
